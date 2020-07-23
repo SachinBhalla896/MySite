a3ee3d975094e9c2f685995cc72c746fbd0d92de
sachin <sachinbhalla896@gmail.com>
2020-07-23 12:39:38 -0700

    Removing

--- src/main/java/com/example/Entity/Student.java
+++ src/main/java/com/example/Entity/Student.java
@@ -82,60 +82,15 @@ public class Student
 		this.rollno = rollno;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((fathername == null) ? 0 : fathername.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((mothername == null) ? 0 : mothername.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((rollno == null) ? 0 : rollno.hashCode());
-		result = prime * result + ((stclass == null) ? 0 : stclass.hashCode());
-		return result;
-	}
+	
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Student other = (Student) obj;
-		if (fathername == null) {
-			if (other.fathername != null)
-				return false;
-		} else if (!fathername.equals(other.fathername))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (mothername == null) {
-			if (other.mothername != null)
-				return false;
-		} else if (!mothername.equals(other.mothername))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (rollno == null) {
-			if (other.rollno != null)
-				return false;
-		} else if (!rollno.equals(other.rollno))
-			return false;
-		if (stclass == null) {
-			if (other.stclass != null)
-				return false;
-		} else if (!stclass.equals(other.stclass))
-			return false;
-		return true;
-	}
+	
+	/*
+	  git cokklvrnnvn rjnv
+	  rvmvovio
+	  vikv
+	  
+	 */
 	
 
 }
